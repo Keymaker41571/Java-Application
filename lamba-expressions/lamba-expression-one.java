4ccc67b1d5fa7f78f2629deda544c68df4a926ca
Keymaker41571 <59454964+Keymaker41571@users.noreply.github.com>
2022-12-24 01:40:08 -0500

    Create lamba-expression-one.java

---
+++ lamba-expressions/lamba-expression-one.java
@@ -0,0 +1,45 @@
+public class LambaExpressionOne {
+	
+	public static void main(String[] args) {
+		
+		School school = (konrad, konradgmail) -> {
+			System.out.println("Name: " + konrad + " & Email: " + konradgmail);
+			return "hi der";	
+				
+		};
+		System.out.println(school.bookAppointment("Konrad", "konradgmail"));
+		School.print();
+		School.show();
+		School.add(5, 2);
+			
+	}
+
+
+@FunctionalInterface
+interface School {	
+	public String bookAppointment(String konrad, String konradgmail);
+	
+	public static void print() {
+		System.out.println("Hey im being called from Functional interface school - via static method");
+	}
+	
+	
+	static void show() {
+		System.out.println("Hey im being called from Functional interface school - via default method method");
+	}
+	
+	public static void add(int x, int y) {
+		int sum = x + y;
+		System.out.println("Simple add method from Functional interface, Sum is: " + sum);
+	}
+	
+}
+}
+
+/* This class show how to use a functional interface on a very basic level
+ * Step 1: create an interface using the @FunctionalInterface notation
+ * Step 2: define its parameters, it has no implementation because it is an abstract method (can only have 1)
+ * Step 3: Create an object of functional interface class and pass parameters in that object
+ * Step 4: Create a Lamba function to call the the object of the functional interface, look up syntax
+ * Note: Lamba functions may or may not have return type, have a declaration, and no access modifier, using lamba () -> { };
+ */
